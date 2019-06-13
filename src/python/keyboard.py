38181dd0537de266d071c0d2a746ddbe817b642b
Itallo Rossi <itallorossi.lucas@gmail.com>
2019-06-14 00:39:24 +0100

    Start migration to python 2.

---
+++ src/python/keyboard.py
@@ -0,0 +1,50 @@
+import sys,tty,termios
+
+class _Getch:
+    def __call__(self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+class Keyboard:
+    UP = "up"
+    DOWN = "down"
+    LEFT = "left"
+    RIGHT = "right"
+    ENTER = "enter"
+    INVALID = "invalid"
+
+    @staticmethod
+    def read():
+        while(True):
+            key = Keyboard.read_char()
+            if key == 3:
+                exit(1)
+            elif key == 65:
+                return Keyboard.UP
+            elif key == 66:
+                return Keyboard.DOWN
+            elif key == 67:
+                return Keyboard.RIGHT
+            elif key == 68:
+                return Keyboard.LEFT
+            elif key == 13:
+                return Keyboard.ENTER
+            elif key == 27 or key == 91:
+                continue
+            else:
+                return Keyboard.INVALID
+
+    @staticmethod
+    def read_char():
+        input_key = _Getch()
+        while(True):
+            key = input_key()
+            if key != '':
+                break
+        return ord(key)
---
+++ src/python/main.py
@@ -0,0 +1,8 @@
+from keyboard import Keyboard
+
+def main():
+    while(True):
+        print Keyboard.read()
+
+if __name__ == "__main__":
+    main()
